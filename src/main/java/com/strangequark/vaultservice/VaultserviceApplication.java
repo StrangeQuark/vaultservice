1e807f6b3bc6ab8dbeeec091d7805df378c4f6f5
StrangeQuark <zacharyzirkle@hotmail.com>
2024-07-02 19:16:22 -0500

    Initialize with fresh Spring Boot project

---
+++ src/main/java/com/strangequark/vaultservice/VaultserviceApplication.java
@@ -0,0 +1,13 @@
+package com.strangequark.vaultservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strangequark/vaultservice/VaultserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strangequark.vaultservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
