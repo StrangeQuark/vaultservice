9377c17ad77681c337ca8dd67d9a7b74cb088c10
StrangeQuark <zacharyzirkle@hotmail.com>
2025-05-18 14:36:16 -0500

    Add Encryption utility

---
+++ src/main/java/com/strangequark/vaultservice/utility/EncryptionUtility.java
@@ -0,0 +1,31 @@
+package com.strangequark.vaultservice.utility;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class EncryptionUtility {
+
+    private static final String ALGORITHM = "AES";
+
+    // Encrypt a given plaintext using AES and the secret key
+    public static String encrypt(String plainText, String secretKey) throws Exception {
+        SecretKey key = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    // Decrypt the given ciphertext using AES and the secret key
+    public static String decrypt(String encryptedText, String secretKey) throws Exception {
+        SecretKey key = new SecretKeySpec(secretKey.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+        return new String(decryptedBytes);
+    }
+}
