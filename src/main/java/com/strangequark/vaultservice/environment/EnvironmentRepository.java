e30e018c0fd8729f0d37c07125b979356811494f
StrangeQuark <zacharyzirkle@hotmail.com>
2025-08-14 20:12:08 -0500

    Add endpoints for getting all envs by service, and getting all services for user

--- src/main/java/com/strangequark/vaultservice/environment/EnvironmentRepository.java
+++ src/main/java/com/strangequark/vaultservice/environment/EnvironmentRepository.java
@@ -2,9 +2,11 @@ package com.strangequark.vaultservice.environment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface EnvironmentRepository extends JpaRepository<Environment, UUID> {
     Optional<Environment> findByNameAndServiceId(String name, UUID serviceId);
+    List<Environment> findAllByServiceId(UUID serviceId);
 }
